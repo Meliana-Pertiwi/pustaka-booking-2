f91e3875c0e2c3e8a1ee49b252c8358dbf1abe7b
Meliana-Pertiwi <melianapertiwi01@gmail.com>
2022-06-08 13:19:05 +0700

    Tugas Pertemuan 7

--- application/views/buku/daftarbuku.php
+++ application/views/buku/daftarbuku.php
@@ -31,11 +31,12 @@
                                 <p>
                                     <?php
                                     if ($buku->stok < 1) {
-                                        echo "<i class='btn btn-outline-primary fas fw fa-shopping-cart'> Booking&nbsp;&nbsp 0</i>";
+                                        echo "<i class=' disabled btn btn-outline-primary fas fw fa-ban' style='color:red'> Stok&nbsp;&nbsp 0</i>";
                                     } else {
                                         echo "<a class='btn btn-outline-primary fas fw fa-shopping-cart' href='" . base_url('booking/tambahBooking/' . $buku->id) . "'> Booking</a>";
                                     }
                                     ?>
+                                    
                                     <a class="btn btn-outline-warning fas fw fa-search" href="<?= base_url('home/detailBuku/' . $buku->id); ?>"> Detail</a></p>
                             </div>
                         </div>
